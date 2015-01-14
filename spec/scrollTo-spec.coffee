d997a8d5ce2d4e80bd2ad8dc5dcbfebbd7ec26f8
ichord <chord.luo@gmail.com>
2015-01-14 20:07:22 +0800

    append specs

--- spec/scrollTo-spec.coffee
+++ spec/scrollTo-spec.coffee
@@ -6,29 +6,59 @@ describe 'Simple scrollTo', ->
       simple.scrollTo()
     expect(testException).toThrow()
 
-  $container = $('<div class="container"></div>').appendTo 'body'
+  $container = null
 
   beforeEach ->
-    $('.container').empty()
+    $container?.remove()
+    $container = $('<div class="container"></div>').appendTo 'body'
 
-  it 'should scroll to a target', ->
-    $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo $container
-    $('<div id="target"></div>').appendTo $container
-    $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo $container
+  describe 'should scroll to a target', ->
+    beforeEach ->
+      $('<div class="blank" style="height: 2000px; width: 600px"></div>').appendTo $container
+      $('<div id="target">I am here</div>').appendTo $container
+      $('<div class="blank" style="height: 2000px; width: 600px"></div>').appendTo $container
 
-    simple.scrollTo
-      target: '#target'
-      animation: false
+    it 'without specific container', ->
+      simple.scrollTo
+        target: '#target'
+        animation: false
+      expect($(document).scrollTop()).toBe parseInt($('#target').offset().top)
 
-    expect($(document).scrollTop()).toBe($('#target').offset().top)
+    it 'with specific container', ->
+      $container.css
+        height: '400px'
+        width: '400px'
+        overflow: 'scroll'
+      simple.scrollTo
+        target: '#target'
+        offset: 20
+        container: $container
+        animation: false
+      expect($('#target').offset().top - $container.offset().top).toBe 20
 
-  it 'should not scroll when the target is visible', ->
-    $(document).scrollTop(0)
-    $('<div class="blank" style="height: 100; width: 100px"></div>').appendTo $container
-    $('<div id="target"></div>').appendTo $container
-    $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo $container
-    simple.scrollTo
-      target: "#target"
-      animation: false
-      actOnVisible: false
-    expect($(document).scrollTop()).toBe(0)
+  describe 'should not scroll when the target is visible', ->
+    beforeEach ->
+      $(document).scrollTop(0)
+      $('<div class="blank" style="height: 100px; width: 500px"></div>').appendTo $container
+      $('<div id="target">I am here</div>').appendTo $container
+      $('<div class="blank" style="height: 2000px; width: 500px"></div>').appendTo $container
+      $container.css
+        height: '400px'
+        width: '400px'
+        overflow: 'scroll'
+
+    it 'without specific container', ->
+      simple.scrollTo
+        target: "#target"
+        animation: false
+        container: $container
+        actOnVisible: false
+      expect($container.scrollTop()).toBe(0)
+
+    it 'with specific container', ->
+      simple.scrollTo
+        target: "#target"
+        container: $container
+        animation: false
+        actOnVisible: false
+      expect($(document).scrollTop()).toBe(0)
--- spec/scrollTo-spec.js
+++ spec/scrollTo-spec.js
@@ -8,31 +8,71 @@
       };
       return expect(testException).toThrow();
     });
-    $container = $('<div class="container"></div>').appendTo('body');
+    $container = null;
     beforeEach(function() {
-      return $('.container').empty();
+      if ($container != null) {
+        $container.remove();
+      }
+      return $container = $('<div class="container"></div>').appendTo('body');
     });
-    it('should scroll to a target', function() {
-      $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo($container);
-      $('<div id="target"></div>').appendTo($container);
-      $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo($container);
-      simple.scrollTo({
-        target: '#target',
-        animation: false
+    describe('should scroll to a target', function() {
+      beforeEach(function() {
+        $('<div class="blank" style="height: 2000px; width: 600px"></div>').appendTo($container);
+        $('<div id="target">I am here</div>').appendTo($container);
+        return $('<div class="blank" style="height: 2000px; width: 600px"></div>').appendTo($container);
       });
-      return expect($(document).scrollTop()).toBe($('#target').offset().top);
+      it('without specific container', function() {
+        simple.scrollTo({
+          target: '#target',
+          animation: false
+        });
+        return expect($(document).scrollTop()).toBe(parseInt($('#target').offset().top));
+      });
+      return it('with specific container', function() {
+        $container.css({
+          height: '400px',
+          width: '400px',
+          overflow: 'scroll'
+        });
+        simple.scrollTo({
+          target: '#target',
+          offset: 20,
+          container: $container,
+          animation: false
+        });
+        return expect($('#target').offset().top - $container.offset().top).toBe(20);
+      });
     });
-    return it('should not scroll when the target is visible', function() {
-      $(document).scrollTop(0);
-      $('<div class="blank" style="height: 100; width: 100px"></div>').appendTo($container);
-      $('<div id="target"></div>').appendTo($container);
-      $('<div class="blank" style="height: 2000px; width: 100px"></div>').appendTo($container);
-      simple.scrollTo({
-        target: "#target",
-        animation: false,
-        actOnVisible: false
+    return describe('should not scroll when the target is visible', function() {
+      beforeEach(function() {
+        $(document).scrollTop(0);
+        $('<div class="blank" style="height: 100px; width: 500px"></div>').appendTo($container);
+        $('<div id="target">I am here</div>').appendTo($container);
+        $('<div class="blank" style="height: 2000px; width: 500px"></div>').appendTo($container);
+        return $container.css({
+          height: '400px',
+          width: '400px',
+          overflow: 'scroll'
+        });
       });
-      return expect($(document).scrollTop()).toBe(0);
+      it('without specific container', function() {
+        simple.scrollTo({
+          target: "#target",
+          animation: false,
+          container: $container,
+          actOnVisible: false
+        });
+        return expect($container.scrollTop()).toBe(0);
+      });
+      return it('with specific container', function() {
+        simple.scrollTo({
+          target: "#target",
+          container: $container,
+          animation: false,
+          actOnVisible: false
+        });
+        return expect($(document).scrollTop()).toBe(0);
+      });
     });
   });
 
